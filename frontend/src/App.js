62e3d9ae06e286c8e30ecd7d075dcf3ffff45c23
claudiachin <claudia.chinsh@gmail.com>
2020-06-13 20:12:45 +0800

    additional front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,76 @@
 import React from 'react';
 import './App.css';
-import { ReactComponent as One } from './1.svg';
-import { ReactComponent as Two } from './2.svg';
-import { ReactComponent as Three } from './3.svg';
-import { ReactComponent as Four } from './4.svg';
-import { ReactComponent as Five } from './5.svg';
-import { ReactComponent as Six } from './6.svg';
+import { ReactComponent as A } from './1.svg';
+import { ReactComponent as B } from './2.svg';
+import { ReactComponent as C } from './3.svg';
+import { ReactComponent as D } from './4.svg';
+import { ReactComponent as E } from './5.svg';
+import { ReactComponent as F } from './6.svg';
+
+class Info extends React.Component {
+  constructor(props) {
+    super(props);
+    this.intervalID = '';
+    this.state = {
+      color: "#8bbf41",
+      section: [{}]*6,
+      fire: "Low",
+    };
+  }
+
+  async componentDidMount() {
+    const url = "https://damp-journey-05387.herokuapp.com/get-data";
+    const response = await fetch(url);
+    const data  = await response.json();
+    this.setState({section: data});
+  }
+
+  handleFireDangerLevel() {
+    let humidity = this.state.section[this.props.id].humidity;
+    let temperature = this.state.section[this.props.id].temperature;
+
+    if (humidity >= 80 && temperature >=35) {
+      this.setState({color:'#b53737',fire:'High'});
+    } else if (humidity >= 75 && temperature >= 33) {
+      this.setState({color:'#ffd300',fire:'Medium'});
+    } else {
+      this.setState({color:'#8bbf41',fire:'Low'});
+    };
+  }
+
+  render() {
+    console.log(this.state.section[this.props.id])
+    return (
+      <div className='Info'>
+          <div>
+            <p className={"Appinfo"}>Area Name: {this.state.section[this.props.id].location}</p>
+            <p className={"Appinfo"}>Area Name: {this.state.section.location}</p>
+            <p className={"Appinfo"}>Humidity: {this.state.section.humidity}%</p>
+            <p className={"Appinfo"}>Temperature: {this.state.section.temperature}&deg;C</p>
+            <p className={"Appinfo"}>Likeliness of fire: {this.state.fire}</p>
+          </div>
+      </div>
+    );
+   }
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.state = {location: 0}
+    this.state = {id: 2, section: []}
   }
 
-  handleColour() {
-    let humidity = 70;
+  async componentDidMount() {
+    const url = "https://damp-journey-05387.herokuapp.com/get-data";
+    const response = await fetch(url);
+    const data  = await response.json();
+    this.setState({section: data});
+  }
+
+  handleColour(id) {
+    console.log(this.state.section[id].humidity)
+    let humidity = this.state.section[id].temperature;
     let temperature = 30;
 
     let fill = '';
@@ -30,19 +85,26 @@ class App extends React.Component {
   }
 
   handleClick(id) {
-    this.setState({location: id});
-    alert('Clicked '+id);
+    this.setState({id: id});
   }
 
   render() {
+    let info = ''
+    if this.state.proceed 
     return (
-      <div className="App" style={{height:window.innerHeight*0.8+'px'}}>
-        <One onClick={this.handleClick.bind(this, 1)} className={this.handleColour()} style={{position:'relative',transform:'scale(0.3)',top:-140+'px',left:-25+'px'}}/>
-        <Two onClick={this.handleClick.bind(this, 2)} className={this.handleColour()} style={{position:'relative',transform:'scale(0.33)',top:-482+'px',left:-138+'px'}}/>
-        <Three onClick={this.handleClick.bind(this, 3)} className={this.handleColour()} style={{position:'relative',transform:'scale(0.48)',top:-838+'px',left:42+'px'}}/>
-        <Four onClick={this.handleClick.bind(this, 4)} className={this.handleColour()} style={{position:'relative',transform:'scale(0.2)',top:-1201+'px',left:101+'px'}}/>
-        <Five onClick={this.handleClick.bind(this, 5)} className={this.handleColour()} style={{position:'relative',transform:'scale(0.37)',top:-1458+'px',left:136+'px'}}/>
-        <Six onClick={this.handleClick.bind(this, 6)} className={this.handleColour()} style={{position:'relative',transform:'scale(0.3)',top:-1635+'px',left:83+'px'}}/>
+      <div>
+        <header style={{height:window.innerHeight*0.1+'px'}}><p id='element'>ELEMENT</p></header>
+        <div style={{display:'float'}}>
+          <div className="Map" style={{height:window.innerHeight*0.8+'px', float:'left'}}>
+            <A onClick={this.handleClick.bind(this, 2)} className={this.handleColour.bind(this, 2)} style={{position:'relative',transform:'scale(0.3)',top:-140+'px',left:-25+'px'}}/>
+            <B onClick={this.handleClick.bind(this, 3)} className={this.handleColour.bind(this, 3)} style={{position:'relative',transform:'scale(0.33)',top:-482+'px',left:-138+'px'}}/>
+            <C onClick={this.handleClick.bind(this, 4)} className={this.handleColour.bind(this, 4)} style={{position:'relative',transform:'scale(0.48)',top:-838+'px',left:42+'px'}}/>
+            <D onClick={this.handleClick.bind(this, 5)} className={this.handleColour.bind(this, 5)} style={{position:'relative',transform:'scale(0.2)',top:-1201+'px',left:101+'px'}}/>
+            <E onClick={this.handleClick.bind(this, 0)} className={this.handleColour.bind(this, 0)} style={{position:'relative',transform:'scale(0.37)',top:-1458+'px',left:136+'px'}}/>
+            <F onClick={this.handleClick.bind(this, 1)} className={this.handleColour.bind(this, 1)} style={{position:'relative',transform:'scale(0.3)',top:-1635+'px',left:83+'px'}}/>
+          </div>
+          <Info id={this.state.id}/>
+        </div>
       </div>
     );
   }
